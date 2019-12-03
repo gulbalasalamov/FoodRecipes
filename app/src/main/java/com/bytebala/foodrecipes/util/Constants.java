b9f454777d16b9a14789713468201b84450dda48
Gulbala Salamov <g.salamov@gmail.com>
2019-12-03 17:45:25 +0000

    Update Constants.java

--- app/src/main/java/com/bytebala/foodrecipes/util/Constants.java
+++ app/src/main/java/com/bytebala/foodrecipes/util/Constants.java
@@ -1,8 +1,11 @@
 package com.bytebala.foodrecipes.util;
 
 public class Constants {
-    public static final String BASE_URL = "https://www.food2fork.com";
-    public static final String API_KEY = "b3e7b58a7b83f71dc1ad8f90b2ab045a";
+    //public static final String BASE_URL = "https://www.food2fork.com";
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    //public static final String API_KEY = "b3e7b58a7b83f71dc1ad8f90b2ab045a";
+    public static final String API_KEY = "";
+
     public static final int NETWORK_TIMEOUT = 3000; //3000ms 3sec
 
     public static String[] DEFAULT_SEARCH_CATEGORIES =
